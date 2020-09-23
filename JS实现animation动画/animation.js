cb123b2c71d7c7481b1232cdce455b7d519c9176
CamilleZxt <1374388481@qq.com>
2020-09-23 23:29:32 +0800

    update

--- JS实现animation动画/animation.js
+++ JS实现animation动画/animation.js
@@ -1,21 +1,19 @@
 
 
-function animation(obj,target,callback){
+function animation(obj, target, callback) {
 	clearInterval(obj.timer);
-	obj.timer = setInterval(function(){
+	obj.timer = setInterval(function () {
 		var step = (target - obj.offsetLeft) / 10;
-		step = step > 0 ? Math.ceil(step): Math.floor(step);
-		if(obj.offsetLeft == target){
-				clearInterval(obj.timer);
-				//定时器停止后，在接着执行下面的操作
-				//所以回调函数的执行写在定时器停止后
-				if (callback) {
-					callback();
-				}
-				// callback&&callback();
+		step = step > 0 ? Math.ceil(step) : Math.floor(step);
+		if (obj.offsetLeft == target) {
+			clearInterval(obj.timer);
+			//定时器停止后，在接着执行下面的操作
+			//所以回调函数的执行写在定时器停止后
+			if (callback) {
+				callback();
+			}
+			// callback&&callback();
 		}
-		obj.style.left = obj.offsetLeft + step +'px';
-	},15);
-
-	
+		obj.style.left = obj.offsetLeft + step + 'px';
+	}, 15);
 }
